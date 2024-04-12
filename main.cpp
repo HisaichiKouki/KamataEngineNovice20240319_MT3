1545bf823c28787781515dac9f88e3d95dad5963
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-04-12 12:12:39 +0900

    Vector3関数を追加

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <Novice.h>
-
+#include "Vector3Math.h"
 const char kWindowTitle[] = "LD2A_01_ヒサイチ_コウキ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	int windowX = 1280;
+	int windowY = 720;
+	Novice::Initialize(kWindowTitle, windowX, windowY);
+
+	Vector3 v1{ 1.0f,3.0f,-5.0f };
+	Vector3 v2{ 4.0f,-1.0f,2.0f };
+	float k = { 4.0f };
+
+	Vector3 resultAdd = Add(v1, v2);
+	Vector3 resultSubtract = Subtract(v1, v2);
+	Vector3 resultMultiply = Multiply(k, v1);
+	float resultDot = Dot(v1, v2);
+	float resultLength = Length(v1);
+	Vector3 resultNormalize = Normalize(v2);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -27,6 +40,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 
+
 		///------------------///
 		/// ↑更新処理ここまで
 		///------------------///
@@ -34,6 +48,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///------------------///
 		/// ↓描画処理ここから
 		///------------------///
+
+		VectorScreenPrintf(0, 0, resultAdd, "Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, "Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, "Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%0.2f :Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%0.2f :Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, "Normalize");
 
 
 
